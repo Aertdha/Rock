e407b5e2762e0f61b53d543cb432fb25c5b3579e
Aertdha <regeditfreetp@gmail.com>
2024-12-25 05:22:21 +0700

    [Aertdha] добавлен CHANGELOG.md

--- Scripts/resources/enemyStats.gd
+++ Scripts/resources/enemyStats.gd
@@ -1,10 +1,10 @@
 extends Resource
 class_name EnemyStats
 
-@export var speed : int = 150  # Было 200-300, уменьшаем
-@export var jump : int = 300   # Можно тоже уменьшить если нужно
+@export var speed : int = 150
+@export var jump : int = 300
 @export var maxHp : int
 var hp = maxHp
 @export var damage : int 
 @export var knockBack_str : Vector2
-@export var max_health : int = 30  # Максимальное здоровье слизи
+@export var max_health : int = 30
